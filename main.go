067c60bd756e13a0606c99d3272fe25e7887ac87
Tristan Van Thielen <tristan.van.thielen@devoteam.com>
2024-05-22 13:50:37 +0200

    Add supported feature

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	pgs "github.com/lyft/protoc-gen-star/v2"
 	pgsgo "github.com/lyft/protoc-gen-star/v2/lang/go"
+	"google.golang.org/protobuf/types/pluginpb"
 
 	"github.com/devoteamgcloud/protoc-gen-gotag/module"
 )
 
 func main() {
-	pgs.Init(pgs.DebugEnv("GOTAG_DEBUG")).
+	supported_features := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	pgs.Init(pgs.DebugEnv("GOTAG_DEBUG"), pgs.SupportedFeatures(&supported_features)).
 		RegisterModule(module.New()).
 		RegisterPostProcessor(pgsgo.GoFmt()).
 		Render()
